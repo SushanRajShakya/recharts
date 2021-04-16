a490b7be9b797b1d6fedffaebcda86b88fc48cbc
Sushan Raj Shakya <sushanshakya005@gmail.com>
2021-04-16 10:21:32 +0545

    master #time 1h Added basic stacked bar chart

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import "./App.css";
 import "./assets/donut-wrapper.css";
+import "./assets/vertical-bar-chart.css";
 
+import BAR_CHART_DATA from "./mocks/bar-chart";
 import DonutChart from "./components/donutChart";
 import DONUT_CHART_DATA from "./mocks/donut-chart";
+import VerticalBarChart from "./components/verticalBarChart";
 
 function App() {
   return (
@@ -11,6 +14,7 @@ function App() {
         data={DONUT_CHART_DATA}
         colorScale={["#4018B1", "#E49F38", "#B11890"]}
       />
+      <VerticalBarChart data={BAR_CHART_DATA} />
     </div>
   );
 }
--- src/config/donutChartConfig.js
+++ src/config/donutChartConfig.js
@@ -2,8 +2,10 @@ export const donutChartConfig = {
   width: 340,
   height: 340,
   dataKey: "value",
+  stroke: "", // Removes the gap betweens slices of pie chart
   nameKey: "label",
   innerRadius: 95,
+  startAngle: -270,
   outerRadius: 120,
   emptyChartDetails: {
     label: "No Data",
